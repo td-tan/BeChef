0c416d4c4b5b8530743fcfbc65a8c4ffa75d3e6d
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-14 16:18:12 +0100

    feat: add imports for login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -14,7 +14,9 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
-    ])
+    ]),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
