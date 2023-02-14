7f35de27d1f1cdfa0afda3f7fb6bb4aa987d2508
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-14 16:20:09 +0100

    refactor: move routes outside for clean imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +16,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: 'login', component: LoginComponent },
-    ]),
+    RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule
   ],
