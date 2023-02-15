4cc66c520d9b28ee1cc015a9bf7696bffbebde72
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-15 11:49:57 +0100

    feat: implement login function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   form:FormGroup;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, 
+    private authService:AuthService, 
+    private router:Router) {
     this.form = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
@@ -17,6 +21,14 @@ export class LoginComponent {
   }
 
   login() {
-    
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password)
+        .subscribe(() => {
+          console.log("User is logged in");
+          this.router.navigateByUrl('/Dashboard');
+        });
+    }
   }
 }
