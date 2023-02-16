922b02faf049fb80f28c7a84c46c316ef641cd08
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-16 20:25:24 +0100

    test: add case for remember me

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -9,7 +11,10 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ ReactiveFormsModule ]
+      imports: [ 
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
@@ -42,6 +47,12 @@ describe('LoginComponent', () => {
 
     expect(inputEmail).toBeTruthy();
     expect(inputPassword).toBeTruthy();
+  });
+
+  it('it should find remember me checkbox', () => {
+    const inputRememberMe: HTMLElement = fixture.nativeElement.querySelector('input#rememberMe');
+
+    expect(inputRememberMe).toBeTruthy();
   });
 
   it('it should find login button', () => {
