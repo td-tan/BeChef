a2e718366fe61a97e76d9f8e3c44d59ae30ce03b
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-16 21:12:24 +0100

    feat: implement route shows on link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BeChef';
+
+  constructor(private router:Router) {}
+
+  isLoginRoute() : Boolean {
+    return this.router.url === '/login';
+  }
 }
