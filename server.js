2fc571916e481346332e75a5102d3d9459a78b97
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-18 14:38:39 +0100

    feat(auth): implement find user in db

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
 require('dotenv').config();
 
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_CONN_URI + '/test')
+  .then(() => {
+    console.log(`⚡️[mongodb]: Server is connected with DB test`);
+  })
+  .catch(error => {
+    console.error('Error connecting to the database:', error);
+});
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
 
 app.post('/api/login', (req, res) => {
   const email = req.body.email,
         password = req.body.password;
-  
-  // Need to validate email and pw
-  console.log(email, password);
-  res.send("Hello World!");
+
+  User.findOne({ email: email}, (err, document) => {
+    if (err) return handleError(err);
+
+    if(Object.is(document, null)) {
+      res.send('Something went wrong.');
+      return;
+    }
+
+    console.log(document.password);
+    res.send(`Hello ${document.email}`);
+  });
 });
 
 app.listen(port, () => {
