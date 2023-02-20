f67344d2958266d454fa6dcbd6d124910311e8b2
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-20 14:07:10 +0100

    refactor: implement catch all error handling in controller

--- server/controller/error.js
+++ server/controller/error.js
@@ -1,11 +1,11 @@
 
 
-function errorhandler(err) {
+function errorhandler(err, req, res) {
     console.log(err)
     res.status(500).send({
       error: 'Something went wrong'
     });
-}
+  }
 
 module.exports = {
     errorhandler
