2fd787563ee7dd05a97f1c87e93dddaa456a0018
td-tan <47648230+td-tan@users.noreply.github.com>
2023-02-24 14:20:28 +0100

    test: add case for banner and submenu

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -8,7 +9,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
 
@@ -20,4 +22,21 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display user welcome banner', () => {
+    const banner: HTMLElement = fixture.nativeElement.querySelector('.banner');
+    expect(banner.textContent).toContain('Hello');
+  });
+
+  it('should display submenu navigation', () => {
+    const subnav: HTMLElement = fixture.nativeElement.querySelector('.subnav');
+    const sections: NodeListOf<HTMLElement> = fixture.nativeElement.querySelectorAll('.subnav section');
+
+    expect(subnav).toBeTruthy();
+    expect(sections?.length).toBe(3);
+    expect(sections[0].textContent).toContain('Leaderboard');
+    expect(sections[1].textContent).toContain('Recipes');
+    expect(sections[2].textContent).toContain('Team');
+  });
+
 });
